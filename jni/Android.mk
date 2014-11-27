9074f9b25b8e3a3c69b3c1dd722c9d133fe248cf
Won-Kyu Park <wkpark@gmail.com>
2014-11-28 00:48:29 +0900

    remove sonic jni files and fix Android.mk for the SoundTouch JNI

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,27 +1,21 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := libsonic_jni
-LOCAL_SRC_FILES := sonicjni.c sonic.c
-#LOCAL_CFLAGS    := -Wall
-LOCAL_LDLIBS    := -llog
+#include stlport headers
+include external/stlport/libstlport.mk
 
-LOCAL_SHARED_LIBRARY := liblog
+LOCAL_MODULE	:= libsoundtouch_jni
 
+LOCAL_C_INCLUDES += external/soundtouch/include
+
+LOCAL_SRC_FILES := SoundTouch.cpp
+
+LOCAL_LDLIBS	+= -llog
+
+LOCAL_CFLAGS	+= -Wall -fvisibility=hidden -I external/soundtouch/include \
+		-U_FORTIFY_SOURCE \
+		-D ST_NO_EXCEPTION_HANDLING -fdata-sections -ffunction-sections -marm
+
+LOCAL_SHARED_LIBRARIES += liblog
+LOCAL_STATIC_LIBRARIES := libsoundtouch
 include $(BUILD_SHARED_LIBRARY)
